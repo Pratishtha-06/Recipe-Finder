46ce0bfd10cba855504e4521e7071bd550e24fa6
Pratishtha <nandwalpratishtha06@gmail.com>
2025-04-12 12:33:47 +0530

    Http changed

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ let searchInput=document.getElementById("search");
 const Recipe = async (query)=>{
   try{
     Recipes.innerHTML=``;
-    const data =await  fetch(`http://www.themealdb.com/api/json/v1/1/search.php?s=${query}`);
+    const data =await  fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`);
     const response = await data.json();
     
     
